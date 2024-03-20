bc6b00db1a4fad37a6fb3b9ca5ea0acdbb6cfc35
Sushant Sharma <sushant.wurk@gmail.com>
2024-03-20 15:28:46 +0530

    take a look at lines thing later, for now we ball

--- chunk.c
+++ chunk.c
@@ -2,17 +2,11 @@
 #include "memory.h"
 #include "value.h"
 
-void init_line_array(Lines_array *lines) {
-    lines->line = NULL;
-    lines->capacity = 0;
-    lines->count = 0;
-}
-
 void init_chunk(Chunk *chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
-    init_line_array(chunk->lines);
+    chunk->lines = NULL;
     init_value_array(&chunk->constants);
 }
 
@@ -22,33 +16,17 @@ void write_chunk(Chunk *chunk, uint8_t byte, int line) {
         chunk->capacity = GROW_CAPACITY(old_capacity);
         chunk->code =
             GROW_ARRAY(uint8_t, chunk->code, old_capacity, chunk->capacity);
+        chunk->lines =
+            GROW_ARRAY(int, chunk->lines, old_capacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
-
-    Lines_array *lines = chunk->lines;
-
-    if (lines->count && lines->line[lines->count - 1].line == line)
-        return;
-
-    if (lines->count < lines->capacity + 1) {
-        int old_capacity = lines->capacity;
-
-        lines->capacity = GROW_CAPACITY(old_capacity);
-
-        lines->line =
-            GROW_ARRAY(Line, lines->line, old_capacity, lines->capacity);
-    }
-
-    lines->line[lines->count].line = line;
-    lines->line[lines->count].offset = chunk->count - 1;
-    lines->count++;
 }
 
 void free_chunk(Chunk *chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    FREE_ARRAY(int, chunk->lines->line, chunk->lines->capacity);
     free_value_array(&chunk->constants);
     init_chunk(chunk);
 }
--- chunk.h
+++ chunk.h
@@ -6,36 +6,20 @@
 
 typedef enum { OP_CONSTANT, OP_RETURN } opCode;
 
-// INFO: this is not pretty but should work? don't quote me on that
-typedef struct {
-    int line;
-    int offset;
-} Line;
-
-typedef struct {
-    Line *line;
-    int count;
-    int capacity;
-} Lines_array;
-
 typedef struct {
     int count;
     int capacity;
     uint8_t *code;
-    Lines_array *lines;
+    int *lines;
     ValueArray constants;
 } Chunk;
 
 void init_chunk(Chunk *chunk);
-
-void init_line_array(Lines_array *lines);
 
 void write_chunk(Chunk *chunk, uint8_t byte, int line);
 
 void free_chunk(Chunk *chunk);
 
 int add_constants(Chunk *chunk, Value value);
-
-int get_line(Lines_array *lines, int idx);
 
 #endif
